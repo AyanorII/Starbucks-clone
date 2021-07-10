42ea7e0eb5010faceefe90683c2588cba72266a8
AyanorII <77108453+AyanorII@users.noreply.github.com>
2021-07-10 12:26:31 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+const starsButtons = document.getElementsByClassName('stars__btn');
+const starsContent = document.getElementsByClassName('stars-free__content');
+const bottomBar = document.querySelector('.bottom-bar');
+
+
 function showContent(evt, index) {
-    const starsButtons = document.getElementsByClassName('stars__btn');
     for (let i = 0; i < starsButtons.length; i++) {
         starsButtons[i].classList.remove('active');
     }
 
-    const starsContent = document.getElementsByClassName('stars-free__content');
     for (let i = 0; i < starsContent.length; i++) {
         starsContent[i].classList.remove('active');
     }
 
     evt.currentTarget.classList.add('active');
     starsContent[index].classList.add('active');
+
+    bottomBar.style.left = `calc(20% * ${index})`;
+
 }
+
 
